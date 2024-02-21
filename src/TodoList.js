69b2d45810c2d92613d5edd634f2750d7a048292
AnmolAgarwalSap <anmol.agarwal02@sap.com>
2024-02-22 04:56:50 +0530

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+
+function TodoList() {
+    const[activity, setActivity]= useState("")
+    const [listData, setlistData] =useState([]);
+    function addActivity(){
+        // setlistData([...listData, activity])
+        // console.log(listData)
+
+        setlistData((listData)=>{
+            const updatedList =[...listData, activity]
+            console.log(updatedList)
+            setActivity('');
+            return updatedList
+        })
+    }
+
+    function removeActivity(i){
+        const updatedListData = listData.filter((elem, id)=>{
+            return i!= id;
+        })
+
+        setlistData(updatedListData);
+
+    }
+  return (
+    <>
+
+    <div className='container'>
+        <div className='header'>
+            TODO LIST
+        </div>
+        <input type='text' placeholder='Add Activity' value={activity} onChange={(e) =>setActivity(e.target.value)}></input>
+        <button onClick={addActivity}>Add</button>
+        <p className='List-heading'> Here is your List </p>
+        {listData!=[] && listData.map((data,i)=>{
+            return(
+                <>
+                <p key={i}>
+                    <div className='listData'>{data}
+                    </div>
+                    <div className='btn-position'><button onClick={()=>removeActivity (i)}>Delete</button>
+                    </div>
+                    </p>
+                    
+                    </>
+            )
+        })}
+
+    </div>
+    </>
+   
+
+   
+  )
+}
+
+export default TodoList
